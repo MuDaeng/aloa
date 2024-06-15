9ba42393170d7721ca713dfec8f9734e8ae2a301
Mudaeng <qkreotjd0588@naver.com>
2024-06-15 12:06:19 +0900

    최초 푸시

---
+++ src/main/java/com/aloa/AloaApplication.java
@@ -0,0 +1,13 @@
+package com.aloa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AloaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aloa/AloaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aloa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
