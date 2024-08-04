8d6b471da342c5641508e0da167c6b47a7dec293
Mudaeng <qkreotjd0588@naver.com>
2024-08-05 01:10:34 +0900

    계산Event 비동기 -> 동기로 변경
    1. Async로 Executor 2개로 돌리려고 했는데 잘안돼서 우선
    executor 제거

--- src/main/java/com/aloa/configuration/ThreadPoolConfig.java
+++ src/main/java/com/aloa/configuration/ThreadPoolConfig.java
@@ -2,14 +2,10 @@ package com.aloa.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ApplicationEventMulticaster;
-import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
-
-import static org.springframework.context.support.AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME;
 
 @Configuration
 @EnableAsync
@@ -17,25 +13,6 @@ public class ThreadPoolConfig {
     final int CORE_POOL_SIZE = 5;
     final int MAX_POOL_SIZE = 10;
     final int QUEUE_CAPACITY = 1000;
-
-    @Bean(name = "calculationExecutor")
-    public Executor calculationExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(CORE_POOL_SIZE);
-        executor.setMaxPoolSize(MAX_POOL_SIZE);
-        executor.setQueueCapacity(QUEUE_CAPACITY);
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.setThreadNamePrefix("calculation-");
-        executor.initialize();
-        return executor;
-    }
-
-    @Bean(name = APPLICATION_EVENT_MULTICASTER_BEAN_NAME)
-    public ApplicationEventMulticaster applicationEventMulticaster() {
-        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
-        eventMulticaster.setTaskExecutor(downloadExecutor());
-        return eventMulticaster;
-    }
 
     @Bean(name = "downloadExecutor")
     public Executor downloadExecutor() {
--- src/main/java/com/aloa/online/video/event/DownloadCompletedEventListener.java
+++ src/main/java/com/aloa/online/video/event/DownloadCompletedEventListener.java
@@ -3,6 +3,7 @@ package com.aloa.online.video.event;
 import com.aloa.common.video.handler.VideoCalculator;
 import com.aloa.common.video.handler.VideoFinder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -14,8 +15,7 @@ public class DownloadCompletedEventListener {
     private final VideoFinder videoFinder;
     private final VideoCalculator videoCalculator;
 
-    @Async("calculationExecutor")
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @EventListener
     public void calculateVideo(DownloadCompletedEvent event){
         var video = videoFinder.findById(event.videoId())
                 .orElseThrow(() -> new IllegalArgumentException("비디오가 미존재"));
